0c8f464291e9250ed9e76bcb06cb330268babcad
tsrajpurohit <156318103+tsrajpurohit@users.noreply.github.com>
2024-11-28 10:12:49 +0530

    Update betavalue.py

--- betavalue.py
+++ betavalue.py
@@ -88,7 +88,7 @@ def calculate_beta(stock, index, period="1y"):
 
 if __name__ == "__main__":
     # Get the credentials path from an environment variable
-    credentials_path = os.getenv("GOOGLE_SHEETS_CREDENTIALS_PATH")
+    credentials_path = os.getenv("GOOGLE_SHEETS_CREDENTIALS")
     if not credentials_path:
         raise ValueError("Environment variable 'GOOGLE_SHEETS_CREDENTIALS_PATH' is not set.")
 
